1a3ed6f54dda0e8fd456adb12a8657c67a3eea9d
eafischer2 <>
2022-06-30 14:19:06 -0800

    Added PRA_postprocessing.py, about to edit.
    (cherry picked from commit a7e7295eef9f8fe8d89b183e33c00eb0b6d00934)

---
+++ sh/PRA_postprocessing.py
@@ -0,0 +1,130 @@
+#PRA_postprocessing.py
+# Created on: October 17th, 2019
+# Yves Buehler, Andreas Stoffel, SLF Davos
+# ---------------------------------------------------------------------------
+
+# Import system modules
+import sys, string, os
+
+import arcpy
+from arcpy import env
+from arcpy.sa import *
+import traceback
+
+
+# Check out any necessary licenses
+arcpy.CheckOutExtension("spatial")
+# Check out ArcGIS 3D Analyst extension license
+arcpy.CheckOutExtension("3d")
+
+
+env.overwriteOutput = True
+
+#Script varibles...
+
+workspace =          arcpy.GetParameterAsText(0) + "\\"
+site =               arcpy.GetParameterAsText(1)
+inShp =              arcpy.GetParameterAsText(2)
+HS_flatfield =       arcpy.GetParameterAsText(3)
+ref_elevation =      arcpy.GetParameterAsText(4)
+gradient_snowdepth = arcpy.GetParameterAsText(5)
+wind_load =          arcpy.GetParameterAsText(6)
+return_period =      arcpy.GetParameterAsText(7)
+Naming =             arcpy.GetParameterAsText(8)
+
+arcpy.AddMessage("workspace = " + workspace)
+arcpy.AddMessage("input Shapefile = " + inShp)
+arcpy.AddMessage("HS delta 3 days = " + HS_flatfield)
+arcpy.AddMessage("return periode = " + return_period)
+
+#-------------------------------------------------------------------------------
+# Export input parameters to csv_file
+import csv
+row1 = ["Site", "Shapefile with delineated PRA", "DHS3_flatfield [m]", "elevation of DHS3_flatfield [m.a.s.l.]", "snow depth increase with elevation [m/100m]", "snow drift [m]", "return period [years]", "Naming (For/NoFor)"]
+row2 = [site, inShp, HS_flatfield, ref_elevation, gradient_snowdepth, wind_load, return_period, Naming]
+array = [row1,row2]
+Name_csv_file = workspace + site
+with open("%s_input_parameters.csv" % Name_csv_file,"w") as f:
+    writer = csv.writer(f,delimiter=";")
+    writer.writerows(array)
+    f.close()
+
+#-------------------------------------------------------------------------------
+# calcualte release volume
+
+# add fields to attribute table
+#arcpy.AddField_management(inShp, "ID", "LONG")
+arcpy.AddField_management(inShp, "d0star", "DOUBLE")
+arcpy.AddField_management(inShp, "slopecorr", "DOUBLE")
+arcpy.AddField_management(inShp, "d0_"+ return_period, "DOUBLE")
+arcpy.AddField_management(inShp, "VOL_" + return_period, "DOUBLE")
+arcpy.AddField_management(inShp, "Wind", "DOUBLE")
+
+# add ID
+#arcpy.CalculateField_management(inShp, 'ID', '!FID!', 'PYTHON3', '#')
+
+# elevation correction
+expression1 = "(" + str(HS_flatfield) + "+((" + "!Mean_DEM! -" + str(ref_elevation) + ")/100* %s))* math.cos(28 * (3.14159/180))" % gradient_snowdepth
+d0star = arcpy.CalculateField_management(inShp, "d0star", expression1, "PYTHON")
+arcpy.AddMessage(expression1)
+
+# slope angle correction (slopecorr)
+expression2 =  "0.291/(math.sin(!Mean_slope!*(3.14159/180)) - 0.202 * math.cos(!Mean_slope!*(3.14159/180)))"
+slopecorr = arcpy.CalculateField_management(inShp, "slopecorr", expression2, "PYTHON", "")
+
+# wind load interpolation between 1000 (0) and 2000 (full wind load) elevation
+# change max windload dependent on scenario!!!
+expression_wind = "Wind_calc(!Mean_DEM!)"
+codeblock = """def Wind_calc(mean_dem):
+    if mean_dem >= 2000:
+        return 0.1
+    elif mean_dem <= 1000:
+        return 0
+    else:
+        return (mean_dem - 1000) * 0.0001""" # windload/1000 also z.B. 0.5/1000 = 0.0005
+
+wind_load2 = arcpy.CalculateField_management(inShp, "Wind", expression_wind, "PYTHON", codeblock)
+
+
+# calculate final d0 (d0_returnperiod)
+expression3 =  "((!d0star! + !Wind!) * !slopecorr! )"
+arcpy.CalculateField_management(inShp, "d0_" + str(return_period), expression3, "PYTHON", "")
+
+# calculate volume (Vol_returnperiod)
+expression3 =  "(!area_m2!/math.cos(!Mean_slope!*(3.14159/180))*!d0_" + str(return_period) + "!)"
+arcpy.CalculateField_management(inShp, "VOL_" + str(return_period), expression3, "PYTHON", "")
+
+#-------------------------------------------------------------------------------
+# categorize avalanches based on release volume
+
+# get resolution
+def unique_values(table, field):
+    with arcpy.da.SearchCursor(table, [field]) as cursor:
+        return sorted({row[0] for row in cursor})
+res = unique_values(inShp,'Scene_reso')
+res = str(int(res[0]))
+
+# get field map
+field_map = arcpy.FieldMappings()
+field_map.addTable(inShp)
+
+# execute categorization
+category_tiny = site + Naming + '_' + res + 'm_' + return_period +'T_rel.shp'
+arcpy.FeatureClassToFeatureClass_conversion(inShp, workspace, category_tiny, '"VOL_%s" < 5000' % (return_period), field_map, '#')
+
+category_small = site + Naming + '_' + res + 'm_' + return_period +'S_rel.shp'
+arcpy.FeatureClassToFeatureClass_conversion(inShp, workspace, category_small, '"VOL_%s" >= 5000 AND "VOL_%s" < 25000' % (return_period, return_period), field_map, '#')
+
+category_medium = site + Naming + '_' + res + 'm_' + return_period +'M_rel.shp'
+arcpy.FeatureClassToFeatureClass_conversion(inShp, workspace, category_medium, '"VOL_%s" >= 25000 AND "VOL_%s" < 60000' % (return_period, return_period), field_map, '#')
+
+category_large = site + Naming + '_' + res + 'm_' + return_period +'L_rel.shp'
+arcpy.FeatureClassToFeatureClass_conversion(inShp, workspace, category_large, '"VOL_%s" >= 60000' % (return_period), field_map, '#')
+
+# delete empty shapefiles
+category_list = [category_tiny, category_small, category_medium, category_large]
+for file in category_list:
+     file_path = workspace + file
+     if arcpy.management.GetCount(file_path)[0] == "0":
+        arcpy.Delete_management(file_path)
+
