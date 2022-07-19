c3cb17b6b712510b3e1465cd4187fb5515ba2cd6
eafischer2 <>
2022-07-19 07:29:36 -0800

    Bug fix
    (cherry picked from commit 1184fff4d25b609f9a596d991eb26be12ba9d281)

--- demo/fetch_project.py
+++ demo/fetch_project.py
@@ -1,16 +1,19 @@
 import dggs.data
 from dggs import avalanche
 from dggs.util import paramutil,harnutil
-import os
+import os,subprocess
 
 scene_dir = dggs.data.join('prj', 'juneau1')
 
 remote_scene_dir = harnutil.remote_linux_name(scene_dir)
+print('remote_scene_dir ',remote_scene_dir)
+hostname = 'antevorta'
 
-files = ['PRA_extreme', 'PRA_frequent', 'scene.cdl', 'scene.nc']
-cmd = ['rsync', 'antevorta', '-avz', #'--from0',
-    '--files-from=-', scene_dir, '{}:{}'.format(hostname, harnutil.remote_linux_name(scene_dir))]
+files = ['PRA_extreme/', 'PRA_frequent/', 'scene.cdl', 'scene.nc']
+cmd = ['rsync', '-avz', #'--from0',
+    '--files-from=-', '{}:{}'.format(hostname, remote_scene_dir), scene_dir]
+print(' '.join(cmd))
 proc = subprocess.Popen(cmd, stdin=subprocess.PIPE)
-p.communicate(input='\n'.join(files))
+proc.communicate(input='\n'.join(files).encode('utf-8'))
 
 
