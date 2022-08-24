db335067acdc2686e775d3c352a361712b7efb18
eafischer2 <>
2022-08-23 18:30:04 -0800

    Added demo file to debug d8graph: python setup.py build; python demo_d8graph.py
    (cherry picked from commit d6a7e3c9cfc7987e28bab660bc7c2d83f4dc2d2e)

---
+++ demo_d8graph.py
@@ -0,0 +1,26 @@
+import d8graph
+from dggs.avalanche import domain
+from uafgi.util import shputil,shapelyutil,gdalutil
+
+pras_file = '/Users/eafischer2/av/prj/juneau1/juneau1_For_5m_30L_rel.shp'
+dem_file = '/Users/eafischer2/av/data/wolken/BaseData_AKAlbers/Juneau_IFSAR_DTM_AKAlbers_EPSG_3338.tif'
+
+# Read DEM
+#grid_info,dem,dem_nodata = gdalutil.read_raster(dem_file)
+grid_info,dem,dem_nodata = domain.dem_example()
+
+
+# Read (one) polygon
+pras_df = shputil.read_df(pras_file)
+row = pras_df.iloc[0]
+print(row)
+pra = row['shape']
+#print('*** ',pra)
+pra_ds = shapelyutil.to_datasource(pra)
+pra_ras = gdalutil.rasterize_polygons(pra_ds, grid_info)
+
+
+
+print(dem)
+
+d8graph.neighbor_graph(dem, dem_nodata, 2)
