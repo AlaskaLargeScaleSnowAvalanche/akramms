d9f4d38aeebe497296543439284840d96b148d62
soa\efischer <eafischer2@alaska.edu>
2022-10-11 17:52:54 -0800

    Long loading removed from default login, make rsync faster and more reliable.

---
+++ sh/prepare_scene.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+source $HOME/opt/conda/m39/etc/profile.d/conda.sh
+#conda activate avalanche
+source $HOME/av/akramms/loadenv
+python $SCRIPT_DIR/prepare_scene.py "$@"
