167eefe822ad6f420449c209ca45dde1a9cc4737
eafischer2 <eafischer2@alaska.edu>
2023-01-26 15:38:54 -0900

    Move harness on Windows to network drive.

--- dggs/data/__init__.py
+++ dggs/data/__init__.py
@@ -1,7 +1,10 @@
 import os
 
 HARNESS = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
-HARNESS_WINDOWS = r'C:\Users\{}\av'.format(os.environ['USER'])
+
+#HARNESS_WINDOWS = r'C:\Users\{}\av'.format(os.environ['USER'])
+HARNESS_WINDOWS = r'\\nona.dnr.state.ak.us\enggeo_projects\avalanche_sim\av'
+
 
 # Convenience function
 def join(*path):
