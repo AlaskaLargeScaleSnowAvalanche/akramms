3525f491215e2712fb692e3c47905723cbc5042e
eafischer2 <eafischer2@alaska.edu>
2023-09-29 18:26:07 -0800

    avalquery should be ready to test
    (cherry picked from commit c7a6c4f0ff5596b67c811ebbed7eeacd9d4b8531)

--- akramms/avalquery.py
+++ akramms/avalquery.py
@@ -15,6 +15,43 @@ def union_extents(extents):
 
     return z0
 # -----------------------------------------------------------------
+#def extents_intersect(ext0, ext1):
+#    """
+#    ext0, ext1: [x0,y0, x1,y1]
+#
+#    See: https://stackoverflow.com/questions/20925818/algorithm-to-check-if-two-boxes-overlap
+#    """
+#    xmin = min(ext0[0], ext1[0])    # .x0
+#    ymin = min(ext0[1], ext1[1])    # .y0
+#
+#    xmax = min(ext0[2], ext1[2])    # .x1
+#    ymax = min(ext0[3], ext1[3])    # .y1
+#
+#    A = [xmin,ymin,xmax,ymax]
+#    B = [xmin,ymin,xmax,ymax]
+#
+#    return not ((A[0] > B[2]) or (A[2] < B[0]) or (A[1] > B[3]) or (A[3] < B[1]))
+
+
+
+def extents_intersect(extent0, extent1):
+    """
+    ext0, ext1: [x0,y0, x1,y1]
+
+    See: https://saturncloud.io/blog/algorithm-to-check-if-two-boxes-overlap/
+    """
+    x1_1, y1_1, x2_1, y2_1 = extent0
+    x1_2, y1_2, x2_2, y2_2 = extent1
+
+    if x2_1 < x1_2 or x2_2 < x1_1:
+        return False
+
+    if y2_1 < y1_2 or y2_2 < y1_1:
+        return False
+
+    return True
+
+# -----------------------------------------------------------------
 def nc_extent(nc_fname, margin=(0.,0.)):
     with netCDF4.Dataset(arc_fname) as nc:
         bbox = nc.variables['bounding_box'][:].reshape(-1)    # [x0,y0,x1,y1]
@@ -23,6 +60,16 @@ def nc_extent(nc_fname, margin=(0.,0.)):
 def add_margin(bbox, margin)
     return [bbox[0]-margin[0], bbox[1]-margin[1], bbox[2]+margin[0], bbox[2]+margin[1]]
 # -----------------------------------------------------------------
+@functools.lru_cache()
+def domain_extents(exp_mod):
+    # Load set of extents
+    domains_shp = os.path.join(exp_mod.dir, f'{exp_mod.name}_domains.shp')
+    df = shputil.read_df(domains_shp, read_shapes=True)
+    for row in df.iterrows():
+        xx,yy = row['shape'].exterior.coords.xy
+        yield row.i,row.j,(xx[0],yy[0], xx[2],yy[2])    # Convert to an Extent-type list of (x0,y0,x1,y1)
+
+
 # -----------------------------------------------------------------
 # ===================================================================
 # "Normalize" means convert whatever the user provided to a list of AvalSpecs
@@ -51,8 +98,11 @@ def _normalize_aval_spec(aspec0):
             # Use user-provided extents, if any
             extent = union_extents(aspec0.extents)    # Our query extent
         else:
-            # Use the default, same extent as the subdomain
-            extent = ...extent of (idom,jdom) subdomain...
+            # Nothing user-provided, use extent of subdomain
+            extent = exp_mod.gridD.subgrid(
+                aspec0.combo.idom, aspec0.combo.jdom,
+                exp_mod.resolution, exp_mod.resolution,
+                margin=False)
 
         # Determine IDs (depends on whether user supplied)
         return [ AvalSpec(aspec0.exp_mod, aspec0.combo, aspec0.ids, [extent]) ]
@@ -65,13 +115,16 @@ def _normalize_aval_spec(aspec0):
         assert len(aspec0.extents) > 0
         assert len(aspec0.ids) == 0
 
-        # Get extent and list of combos intersecting that extent
+        # Get extent
         extent = union_extents(aspec0.extents)    # Our query extent
+
+        # Get list of combos intersecting that extent
         aspec1s = list()
-        for idom,jdom in exp_mod.gridD.domains_intersecting_extent(extent):
-            lcombo = list(combo[:-2]) + [idom, jdom]
-            combo = exp_mod.Combo(*lcombo)
-            aspec1s.append(AvalSpec(aspec0.exp_mod, combo, [], [extent]))
+        for idom,jdom,dom_ext in domain_extents(exp_mod):
+            if extents_intersect(dom_ext, extent):
+                lcombo = list(combo[:-2]) + [idom, jdom]
+                combo = exp_mod.Combo(*lcombo)
+                aspec1s.append(AvalSpec(aspec0.exp_mod, combo, [], [extent]))
 
         return aspec1s
 
