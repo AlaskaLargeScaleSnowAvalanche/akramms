2b49b7cccc0dbd62324e544f29227bec66a96156
eafischer2 <eafischer2@alaska.edu>
2023-12-13 13:26:03 -0900

    changes

--- akramms/r_prepare.py
+++ akramms/r_prepare.py
@@ -299,10 +299,10 @@ def _data_prep_PRA2(vv, Slope_lowerlimit, name_scenario):
     #     CurvBinary = Con((arcpy.sa.Raster(Curv_plan) <
     #         (-1*float(Curv_upperlimit))) |
     #         (arcpy.sa.Raster(Curv_plan) > float(Curv_upperlimit)), 0, 1)
-    Curve_r = gdalutil.read_raster(vv.Curv_plan)
-    Curve_in = np.logical_and(
-        Curve_r.data >= -vv.Curv_upperlimit,
-        Curve_r.data <=  vv.Curv_upperlimit)
+    Curv_r = gdalutil.read_raster(vv.Curv_plan)
+    Curv_in = np.logical_and(
+        Curv_r.data >= -vv.Curv_upperlimit,
+        Curv_r.data <=  vv.Curv_upperlimit)
 
     # create Ruggedness binary
     # RuggednessBinary = Con((Ruggedness > float(Rugged_upperlimit)), 0, 1)
