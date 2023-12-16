323bff827694f2f43d4bd67b3ac2c9f0805665c2
eafischer2 <eafischer2@alaska.edu>
2023-12-15 17:59:50 -0900

    changes

--- demo/dom.py
+++ demo/dom.py
@@ -34,7 +34,7 @@ def main():
 
     df = shputil.read_df(pra_frequent_file, shape='pra')
     df = df.rename(columns={'fid': 'Id'})    # RAMMS etc. want it named "Id"
-    row = df[df.Id == 4857]
+    row = df[df.Id == 4857].squeeze()
     print(row)
     print(type(row))
 
