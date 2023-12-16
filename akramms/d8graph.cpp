0669b7e0e3757bf0105129e3db7144f6137a3a24
eafischer2 <eafischer2@alaska.edu>
2023-12-15 18:32:47 -0900

    changes

--- akramms/d8graph.cpp
+++ akramms/d8graph.cpp
@@ -1279,6 +1279,14 @@ static PyObject* d8graph_find_domain(PyObject *module, PyObject *args, PyObject 
             std::array<double,2>{x+dx,y-dy},
             std::array<double,2>{x-dx,y-dy}};
         ret_mbr = polygon_to_python(mbr);
+
+        if (debug) {
+            return PyTuple_Pack(3, ret_seen, ret_mbr, ret_mbr);
+        } else {
+            return ret_mbr;
+        }
+
+
     }
 
     if (debug) {
