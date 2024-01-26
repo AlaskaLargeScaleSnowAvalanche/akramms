f529067808c2b5c87a1ac3ecba1b8ef215d1040f
eafischer2 <eafischer2@alaska.edu>
2024-01-25 17:47:38 -0900

    changes

--- akramms/archive.py
+++ akramms/archive.py
@@ -215,7 +215,7 @@ class OverrunChecker:
     def __init__(self, dem_mask_tif):
         self.gridI,dem_mask,_ = gdalutil.read_raster(dem_mask_tif)
         self.dem_mask = dem_mask.astype(np.byte)
-        print('dddddddddddd ', self.dem_mask.dtype)
+        print('dddddddddddd ', self.dem_mask.dtype, self.dem_mask.shape)
 
     def is_overrun(self, in_zip, out_zip):
         """Determines whether a RAMMS result is overrun
