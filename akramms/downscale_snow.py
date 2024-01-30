17fd39d73ce86ca0eb54698671dd8f893b55d165
eafischer2 <eafischer2@alaska.edu>
2024-01-29 15:03:24 -0900

    changes

--- akramms/downscale_snow.py
+++ akramms/downscale_snow.py
@@ -37,7 +37,7 @@ def snowfile_vrt(snowfile_argss):
 
     snowfiles = [snowfile(*x) for x in snowfile_argss]
     snowdir = snowfiles[0].parents[0]
-    snowfile_glob = str(snowfile(*(snowfile_argss[0][:-2] + ('*','*'))))
+    snowfile_glob = snowfile(*(snowfile_argss[0][:-2] + ('*','*')))
     ofname = snowfile_glob.parents[0] / (snowfile_glob.parts[-1][:-8] + '.vrt')  # _*_*.tif -> .vrt
 
     print(snowfiles)
@@ -65,7 +65,7 @@ def snowfile_vrt(snowfile_argss):
         # Include ALL appropriate snowfiles in the VRT, not just the
         # snowfile(s) needed for now.
         cmd.append(ofname)
-        cmd += [os.path.relname(x, snowdir) for x in glob.glob(snowfile_glob)]
+        cmd += [os.path.relname(x, snowdir) for x in glob.glob(str(snowfile_glob))]
         subprocess.run(cmd, cwd=snowdir, check=True)
 
     return ofname
