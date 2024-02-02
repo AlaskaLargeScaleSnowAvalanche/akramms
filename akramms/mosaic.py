91e83fd1addb360f98c5702511711476723b3cff
eafischer2 <eafischer2@alaska.edu>
2024-02-01 16:23:37 -0900

    changes

--- akramms/mosaic.py
+++ akramms/mosaic.py
@@ -4,7 +4,7 @@ import pandas as pd
 from osgeo import gdal
 import zipfile,netCDF4
 from uafgi.util import gdalutil,cfutil,shputil
-from akramms import experiment,archive
+from akramms import experiment,archive,file_info
 import _mosaic
 
 # python -m cProfile -o prof -s cumtime `which akramms` mosaic juneau1-1981-1990.qy 
