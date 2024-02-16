686070349459d3fa46089cf77af2d06bc1612b45
eafischer2 <eafischer2@alaska.edu>
2024-02-16 12:44:59 -0900

    Move For/NoFor to inner-most loop instead of outer, so adjacent For/NoFor runs are done at similar times for the same tile

--- akramms/experiment/ak.py
+++ akramms/experiment/ak.py
@@ -221,13 +221,10 @@ def full():
     # Generate set of trials
     snow = 'ccsm'
     downscale_algo = 'lapse'
-#    for forest in ('For', 'NoFor'):
-    for forest in ('NoFor','For'):
-        for idom,jdom in spiral_domains(113, 45):    # Spiral around Juneau
-            for year0,year1 in [(1981, 2010),(2031,2060)]:        # 30-year climatologies
-#            for year0,year1 in [(1981, 1990), (2051,2060)]:    # 10-year climatologies
-                for return_period in [10,30,100,300]:
-#                for return_period in [10,100]:
+    for idom,jdom in spiral_domains(113, 45):    # Spiral around Juneau
+        for year0,year1 in [(1981, 2010),(2031,2060)]:        # 30-year climatologies
+            for return_period in [10,30,100,300]:
+                for forest in ('NoFor','For'):
                     yield Combo(snow, year0, year1, downscale_algo, forest, return_period, idom, jdom)
 
 # -----------------------------------------------------------------
