022bba31ae2db0322260f64f38c755d57e89d225
eafischer2 <eafischer2@alaska.edu>
2024-03-20 17:38:31 -0800

    changes

--- akramms/avaldb.py
+++ akramms/avaldb.py
@@ -13,7 +13,7 @@ def init_db_sql():
 def create_tables_sql(expmod):
 
     exp = expmod.name
-    cols = [('exp', 'varchar(20)')] + [(key, expmod.combo_sql_types[key]) for key in expmod.combo_schema.keys()]
+    cols = [('exp', 'varchar(20)')] + [(key, expmod.combo_sql_types[key]) for key in expmod.combo_schema.schema.keys()]
     cols = cols[:-2]    # Exclude idom/jdom
     cols_sql = ','.join('{col}' for col,_ in cols)
     cols_types_sql = ','.join('{col} {dtype} not null' for col,dtype in cols)
