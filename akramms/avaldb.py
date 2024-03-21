1f030ce432a8d6cd32168518f7e1dcaefb692f13
eafischer2 <eafischer2@alaska.edu>
2024-03-20 17:36:53 -0800

    changes

--- akramms/avaldb.py
+++ akramms/avaldb.py
@@ -1,27 +1,45 @@
 # Stuff to create the PostGIS database
 
-def create_tables_sql(expmod):>
+
+def init_db_sql():
+    return """
+        -- Must be done from postgres user
+        CREATE EXTENSION postgis;
+        -- https://www.postgresql.org/docs/current/btree-gist.html
+        create extension btree_gist;
+    """:
+
+
+def create_tables_sql(expmod):
 
     exp = expmod.name
     cols = [('exp', 'varchar(20)')] + [(key, expmod.combo_sql_types[key]) for key in expmod.combo_schema.keys()]
+    cols = cols[:-2]    # Exclude idom/jdom
     cols_sql = ','.join('{col}' for col,_ in cols)
     cols_types_sql = ','.join('{col} {dtype} not null' for col,dtype in cols)
 
     sql = f"""
 
         -- List the combos in this experiment
-        create table {exp}_combos (
-            comboid serial,
+        create table {exp}_wcombos (
+            wcomboid serial,
             {cols_types_sql},
             PRIMARY KEY({cols_sql});
 
-        -- Store avalanches by combo
+        -- Keep track of which combos we've uploaded
         create table {exp}_combos (
-            comboid serial,
+            wcomboid int,
+            idom int,
+            jdom int,
+            PRIMARY KEY(wcomboid, idom, jdom));
+
+        -- Store avalanches by wcombo
+        create table {exp}_avals (
+            wcomboid serial,
             avlaid int,
-            UNIQUE KEY(comboid, avalid));
+            UNIQUE KEY(wcomboid, avalid));
         SELECT AddGeometryColumn('{exp}_avals', 'extent', '{expmod.epsg}', 'MULTIPOLYGON', 2);
-        CREATE INDEX {exp}_extent_idx ON {exp}_combos USING GIST (comboid, extent);
+        CREATE INDEX {exp}_extent_idx ON {exp}_avals USING GIST (wcomboid, extent);
     """
 
 
