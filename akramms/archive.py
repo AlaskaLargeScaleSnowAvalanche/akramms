678d6b74673188d40b73ba4fdf89908cd1d1d79a
eafischer2 <eafischer2@alaska.edu>
2024-03-28 17:03:07 -0800

    changes

--- akramms/archive.py
+++ akramms/archive.py
@@ -698,7 +698,7 @@ def finish_combo(expmod, combo, dry_run=False):
             # Read avalanches, compute extent, and write into extent file
             nrow = len(akdf)
             n = 0
-            print('Polygonizing {nrow} avalanche extents', end='')
+            print(f'Polygonizing {nrow} avalanche extents', end='')
             sys.stdout.flush()
             for tup in akdf.sort_values('id').itertuples(index=False):
                 if n%100 == 0:
