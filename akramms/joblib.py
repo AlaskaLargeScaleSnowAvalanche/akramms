777b47e32663757809d4b68864c96e64661fce4d
eafischer2 <eafischer2@alaska.edu>
2024-04-02 20:06:30 -0800

    changes

--- akramms/joblib.py
+++ akramms/joblib.py
@@ -542,7 +542,7 @@ def add_combo_status(akdf0, realized=True, update=True, dry_run=False, ignore_st
         # (which presumably have fixed overrun problems)
         iddf1 = overrun.drop_duplicates(iddf1)
         print('zzzzzzzzzzz')
-        print(iddf1['combo','id', 'id_status'])
+        print(iddf1[['combo','id', 'id_status']])
 
         # Aggregate id status back to combo level and add to akdf1
         # (Now we know whether the combo has fully finished)
