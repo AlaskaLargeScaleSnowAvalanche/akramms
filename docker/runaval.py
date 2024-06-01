0a2d16c902cea1d321da2a352cb518e3c3548b07
efischer <eafischer2@alaska.edu>
2024-06-01 11:48:32 -0800

    Changes, run candidate for runaval.py

--- docker/runaval.py
+++ docker/runaval.py
@@ -8,7 +8,7 @@ import sys,re,subprocess,os,gzip,shutil,zipfile,traceback,pathlib,datetime,struc
 
 # --------------------------------------------------------------
 def parse_xy_coord_raw(fin):
-    """Read the .xy-coord file"""
+    """Read the .xy-coord file.  Just return LENGTHS of the arrays"""
 
     # https://docs.python.org/3/library/struct.html
     # < = little-endian
@@ -23,12 +23,17 @@ def parse_xy_coord_raw(fin):
 
 
     # xvec: double64[ncells]
+    lens = list()
     buf = fin.read(ncells * 8)
-    xvec = np.frombuffer(buf, dtype='<f8')
-    buf = fin.read(ncells * 8)
-    yvec = np.frombuffer(buf, dtype='<f8')
+#    xvec = np.frombuffer(buf, dtype='<f8')
+    lens.append(('xvec', len(buf) // 8))
 
-    return xvec, yvec
+    buf = fin.read(ncells * 8)
+#    yvec = np.frombuffer(buf, dtype='<f8')
+    lens.append(('yvec', len(buf) //8))
+
+    return lens
+#    return xvec, yvec
 
 def parse_out(fin, zipname=None, check=True):
     """Read the .out file"""
@@ -39,23 +44,20 @@ def parse_out(fin, zipname=None, check=True):
     ncells = struct.unpack(fmt, buf)[0]
 #    print('parse_out() ncells = ', ncells)
 
+    lens = list()
     buf = fin.read(ncells * 4)
-    max_vel = np.frombuffer(buf, dtype='<f4')
-    buf = fin.read(ncells * 4)
-    max_height = np.frombuffer(buf, dtype='<f4')
-    buf = fin.read(ncells * 4)
-    depo = np.frombuffer(buf, dtype='<f4')
+#    max_vel = np.frombuffer(buf, dtype='<f4')
+    lens.append(('max_vel', len(buf) // 4))
 
-    # Check that all lengths match!
-    if check:
-        vecs = [max_vel, max_height, depo]
-        if not all((vecs[0].shape == vec.shape for vec in vecs[1:])):
-            raise ValueError(f'ERROR reading .OUT IN {zipname}: lengths are {[vec.shape for vec in vecs]}')
+    buf = fin.read(ncells * 4)
+#    max_height = np.frombuffer(buf, dtype='<f4')
+    lens.append(('max_height', len(buf) // 4))
 
-    return (
-        ('max_vel', max_vel, {'units': 'm s-1'}),
-        ('max_height', max_height, {'units': 'm'}),
-        ('depo', depo,  {'units': 'm'}))
+    buf = fin.read(ncells * 4)
+#    depo = np.frombuffer(buf, dtype='<f4')
+    lens.append(('depo', len(buf) // 4))
+
+    return lens
 
 # --------------------------------------------------------------
 
@@ -132,16 +134,14 @@ try:
     # Check that input and output array lengths match
     fname = f'{log_base}.xy-coord'
     with open(fname, 'rb') as fin:
-        xvec, yvec = archive.parse_xy_coord_raw(fin)
-        vecs = [('xvec', fname, xvec), ('yvec', fname, yvec)]
+        lens = parse_xy_coord_raw(fin)
 
     fname = f'{log_base}.out'
     with open(fname, 'rb') as fin:
-        for name, val, attrs in archive.parse_out(fin):
-            vecs.append((name, fname, val))
+        lens += parse_out(fin)
 
-    if not all(vecs[0][2].shape == vec[2].shape for vec in vecs[1:]):
-        raise ValueError('Inconsistent shape of input / output: {vecs}')
+    if not all(lens[0][1] == xlen[2] for xlen in lens[1:]):
+        raise ValueError('Inconsistent shape of input / output: {lens}')
 
 
     # We were successful... add outputs to our zip
