e7b2dc093829d84a5b92741f2d012c7cb463b928
efischer <eafischer2@alaska.edu>
2024-08-21 16:51:32 -0800

    Beginning work on fig04

--- figures/fig04.py
+++ figures/fig04.py
@@ -4,12 +4,15 @@ import cartopy.io.img_tiles
 from akramms import config
 import matplotlib.pyplot as plt
 import akramms.experiment.ak as exp
+from uafgi.util import gdalutil
 from akfigs import *
 
 
 # \caption{Map of the study area including the Alaska panhandle.  The todo-color outline shows the domain used to run WRF to generate estimates of maximum snow depth.  The todo-color squares show \SI{30}{\kilo\meter} square \emph{tiles} used to divide the overall domain into computationally managable pieces.}
 
 # Line2D Properties: https://matplotlib.org/stable/api/_as_gen/matplotlib.lines.Line2D.html#matplotlib.lines.Line2D
+
+ccsm_tif = pathlib.Path(os.environ['HOME']) / 'av/data/lader/sx3' / 'ccsm_2010_sx3.tif'
 
 def main():
     map_crs = cartopy.crs.epsg(3338)    # Alaska Albers
@@ -21,14 +24,22 @@ def main():
         figsize=(8.5,5.5))
     ax.set_extent(map_extent, map_crs)
 
-    ax.add_image(cartopy.io.img_tiles.OSM(cache=True), 7, alpha=1)    # Use level 7 (lower # is coarser)
+
+    snow_grid, snow_data, snow_nd = gdalutil.read_raster(ccsm_tif)
+    pcm_elev = ax.pcolormesh(
+        snow_grid.centersx, snow_grid.centersy, snow_data,
+        alpha=0.5, rasterized=True,
+        transform=map_crs)#, cmap=cmap, vmin=0, vmax=2000)
+
+
+#    ax.add_image(cartopy.io.img_tiles.OSM(cache=True), 7, alpha=1)    # Use level 7 (lower # is coarser)
 #    ax.coastlines(resolution='50m', color='grey', linewidth=0.5)
 
-    shape_feature = cartopy.feature.ShapelyFeature(
-        cartopy.io.shapereader.Reader(str(exp.dir / 'ak_domains.shp')).geometries(),
-        map_crs)
-
-    ax.add_feature(shape_feature, facecolor='pink', alpha=.3, edgecolor='black', lw=0.3)
+#    shape_feature = cartopy.feature.ShapelyFeature(
+#        cartopy.io.shapereader.Reader(str(exp.dir / 'ak_domains.shp')).geometries(),
+#        map_crs)
+#
+#    ax.add_feature(shape_feature, facecolor='pink', alpha=.3, edgecolor='black', lw=0.3)
 
 
     # Plot Juneau and other cities
@@ -40,8 +51,8 @@ def main():
         ax.text(lon, lat, f'  {city_name}', transform=cartopy.crs.PlateCarree(),
             fontdict = {'size': 4, 'color': 'black'})
 
-    ofname = pathlib.Path('./fig01.png')
-    with TrimmedPng(ofname) as tname:
+    ofname = pathlib.Path('./fig04.pdf')
+    with TrimmedPdf(ofname) as tname:
         fig.savefig(tname, dpi=300, bbox_inches='tight', pad_inches=0.5)   # Hi-res version; add margin so text is not cut off
 
 main()
