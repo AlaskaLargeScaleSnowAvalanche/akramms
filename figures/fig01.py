201d6e0dc8a50ae122a80c80d447be39b88bbc91
efischer <eafischer2@alaska.edu>
2024-09-26 16:12:49 -0800

    Bounds on fig01 now good

--- figures/fig01.py
+++ figures/fig01.py
@@ -18,16 +18,26 @@ geo_nc = ccsm_dir / 'geo_southeast.nc'    # Describes grid
 
 def main():
     map_crs = cartopy.crs.epsg(3338)    # Alaska Albers
-    map_extent = ((320-100)*1000, (1510+300)*1000, (710-300)*1000, (1425+100)*1000)    # xmin, xmax, ymin, ymax; ymin in South
-#    map_extent = ((320-160)*1000, (1510+300)*1000, (710-390)*1000, (1425+200)*1000)    # xmin, xmax, ymin, ymax; ymin in South
+#    map_crs._y_limits = (200000, map_crs.y_limits[1])
 
-#    map_extent = (-320000, 1510*1000, 110000, 1425*1000)    # xmin, xmax, ymin, ymax; ymin in South
+    # HACK: Increase the bounds that this projection is allowed to display
+    bb = map_crs.bounds
+    map_crs.bounds = (bb[0], bb[1]+100*1000, bb[2]-100*1000, bb[3])
+    print('bounds ', map_crs.bounds)
+
+#    map_crs = cartopy.crs('ESRI:102008')
+#    map_extent = ((320-100)*1000, (1510+300)*1000, (710-300)*1000, (1425+100)*1000)    # xmin, xmax, ymin, ymax; ymin in South
+    map_extent = ((320-180)*1000, 1670*1000, 300*1000, (1425+230)*1000)    # xmin, xmax, ymin, ymax; ymin in South
+#    map_extent = ((320-180)*1000, (1510+360)*1000, 300000, (1425+250+500)*1000)    # xmin, xmax, ymin, ymax; ymin in South
+
+#    map_extent = (-320000, 1510*1000, 1100*1000, 1425*1000)    # xmin, xmax, ymin, ymax; ymin in South
+    print('map_extent ', map_extent)
 
     fig,ax = plt.subplots(
         nrows=1,ncols=1,
         subplot_kw={'projection': map_crs},
-        figsize=(8.5,5.5))
-    ax.set_extent(map_extent, map_crs)
+        figsize=(8.5,11.))
+    ax.set_extent(map_extent, crs=map_crs)
 
     ax.add_image(cartopy.io.img_tiles.OSM(cache=True), 7, alpha=1)    # Use level 7 (lower # is coarser)
 #    ax.coastlines(resolution='50m', color='grey', linewidth=0.5)
@@ -53,7 +63,8 @@ def main():
             marker='*', markersize=2, color='black', alpha=0.9))
 
     ofname = pathlib.Path('./fig01.pdf')
-    with akfigs.TrimmedPdf(ofname) as tname:
-        fig.savefig(tname, dpi=300, bbox_inches='tight', pad_inches=0.5)   # Hi-res version; add margin so text is not cut off
+    fig.savefig(ofname, dpi=300)   # Hi-res version; add margin so text is not cut off
+#    with akfigs.TrimmedPdf(ofname) as tname:
+#        fig.savefig(tname, dpi=300, bbox_inches='tight', pad_inches=0.5)   # Hi-res version; add margin so text is not cut off
 
 main()
